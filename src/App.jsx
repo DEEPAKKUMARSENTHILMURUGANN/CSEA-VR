86fd9415c5d42085ba43d1a0ba984fe6444455fc
Dharanesh777 <kdharanesh14@gmail.com>
2026-07-01 21:31:25 +0530

    Added Zone3 technology pages with icons and swiper gallery

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,29 @@
-import { useState } from 'react'
-import Zone2 from './components/zones/Zone2_StudentInnovationGallery/Zone2'
-import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Zone3 from "./components/zones/Zone3/Zone3";
+import TechnologyPage from "./components/zones/Zone3/Technologypage";
+import LabPage from "./components/zones/Zone3/LabPage";
+
+import "./App.css";
 
 function App() {
   return (
-    <>
-      <Zone2 />
-    </>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Zone3 />} />
+
+        <Route
+          path="/technology/:slug"
+          element={<TechnologyPage />}
+        />
+
+        <Route
+          path="/technology/:slug/lab/:labId"
+          element={<LabPage />}
+        />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
